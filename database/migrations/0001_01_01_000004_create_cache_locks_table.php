6d3e8b5c5840fd504e4123774ed00b413363cf6d
Josh Manders <josh@joshmanders.com>
2024-11-14 23:00:26 -0600

    update database stuff

--- database/migrations/0001_01_01_000004_create_cache_locks_table.php
+++ database/migrations/0001_01_01_000004_create_cache_locks_table.php
@@ -4,21 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->mediumText('value');
-            $table->integer('expiration');
-        });
-
         Schema::create('cache_locks', function (Blueprint $table) {
             $table->string('key')->primary();
+
             $table->string('owner');
             $table->integer('expiration');
         });
@@ -29,7 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cache');
         Schema::dropIfExists('cache_locks');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => env('DATABASE_SEEDER_NAME', 'Test User'),
+            'email' => env('DATABASE_SEEDER_EMAIL', 'test@example.com'),
+            'confirmed_at' => now(),
         ]);
     }
 }
