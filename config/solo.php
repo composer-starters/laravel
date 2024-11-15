f8c9b319fe9854ce855add9cbe8a408b8b93ae45
Josh Manders <josh@joshmanders.com>
2024-11-14 20:03:00 -0600

    add solo

---
+++ config/solo.php
@@ -0,0 +1,27 @@
+<?php
+
+use AaronFrancis\Solo\Commands\EnhancedTailCommand;
+
+return [
+    'theme' => 'dark',
+
+    // Commands that auto start.
+    'commands' => [
+        EnhancedTailCommand::make('Logs', 'tail -f -n 100 ' . storage_path('logs/laravel.log')),
+        'Vite' => 'npm run dev --silent',
+    ],
+
+    // Not auto-started
+    'lazyCommands' => [
+        'HTTP' => 'php artisan serve',
+        'Queue' => 'php artisan queue:listen --tries=1',
+        'Scheduler' => 'php artisan schedule:work',
+        'Reverb' => 'php artisan reverb:start',
+        'Pint' => 'vendor/bin/pint --ansi',
+    ],
+
+    // FQCNs of trusted classes that can add commands.
+    'allowedCommands' => [
+        //
+    ],
+];
