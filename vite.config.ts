0bc549a62edd363adff2923d7075a805672f9a23
Josh Manders <josh@joshmanders.com>
2025-03-11 22:41:15 -0500

    update to tailwind 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
   plugins: [
+    tailwindcss(),
     tsconfigPaths(),
     laravel({
-      input: ['resources/client/app.css', 'resources/client/app.ts'],
+      input: ['resources/client/tailwind.css', 'resources/client/app.ts'],
       refresh: true,
     }),
   ],
